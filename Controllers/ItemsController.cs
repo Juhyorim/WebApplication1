22e15caf4c52b5b2110628e452ca81a7cda09b3c
Juhyorim <joohr1234@gmail.com>
2025-03-20 02:58:10 +0900

    initial commit

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ItemsController : Controller //base controller를 상속받음 필수로
+    {
+        public IActionResult Overview()
+        {
+            var item = new Item() { Name = "mouse" };
+
+            return View(item);
+        }
+
+        //http://localhost:5076/items/edit/1
+        //http://localhost:5076/items/edit?id=4
+        public IActionResult Edit(int id) //pattern을 id로 지정했기 때문에 다른 이름으로 하면 안됨. edit/1이 안먹힘
+        {
+            return Content("id = " + id);
+        }
+    }
+}
---
+++ Data/MyAppContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    //main bridge that connects our project with the db
+    public class MyAppContext : DbContext
+    {
+        public MyAppContext(DbContextOptions<MyAppContext> options) : base(options) { }
+
+        public DbSet<Item> Items { get; set; }
+    }
+}
