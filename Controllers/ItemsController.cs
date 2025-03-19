bf103ee3777c4632f93647d2a7c59b4dbfcce5f7
Juhyorim <joohr1234@gmail.com>
2025-03-20 03:38:05 +0900

    feat: create item & redirect index.cshtml page

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -1,22 +1,56 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
 using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
     public class ItemsController : Controller //base controller를 상속받음 필수로
     {
-        public IActionResult Overview()
+        private readonly MyAppContext _context;
+
+        public ItemsController(MyAppContext context)
         {
-            var item = new Item() { Name = "mouse" };
+            _context = context;
+
+        }
+
+        //메인페이지에 아이템 다 나오게 ㅇㅇ select
+        public async Task<IActionResult> Index()
+        {
+            var item = await _context.Items.ToListAsync();
 
             return View(item);
         }
 
-        //http://localhost:5076/items/edit/1
-        //http://localhost:5076/items/edit?id=4
-        public IActionResult Edit(int id) //pattern을 id로 지정했기 때문에 다른 이름으로 하면 안됨. edit/1이 안먹힘
+        public IActionResult Create()
         {
-            return Content("id = " + id);
+            return View();
         }
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("Id, Name, Price")] Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Items.Add(item);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+        }
+
+        //이전 학습 코드
+        //public IActionResult Overview()
+        //{
+        //    var item = new Item() { Name = "mouse" };
+
+        //    return View(item);
+        //}
+
+        ////http://localhost:5076/items/edit/1
+        ////http://localhost:5076/items/edit?id=4
+        //public IActionResult Edit(int id) //pattern을 id로 지정했기 때문에 다른 이름으로 하면 안됨. edit/1이 안먹힘
+        //{
+        //    return Content("id = " + id);
+        //}
     }
 }
