78d81bece8bf23ab28736a26a07d762a3a772203
Juhyorim <joohr1234@gmail.com>
2025-03-21 00:47:06 +0900

    feat: create board 기능 개발

---
+++ Controllers/Limeboard/BoardController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication1.Models.limeboard;
+using Microsoft.Extensions.Logging;
+
+
+namespace WebApplication1.Controllers.Limeboard
+{
+    public class BoardController : Controller
+    {
+        private readonly ILoggerFactory factory = LoggerFactory.Create(builder => builder.AddConsole());
+
+        private readonly MyAppContext _context;
+
+        public BoardController(MyAppContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var Boards = await _context.Boards.Include(b => b.Member).ToListAsync();
+
+            return View(Boards);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("Title, Content")] Board board)
+        {
+            ILogger logger = factory.CreateLogger("Program");
+
+            board.MemberId = 1;
+
+
+            if (ModelState.IsValid)
+            {
+                logger.LogInformation("@@@@@@isValid");
+
+                _context.Boards.Add(board);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            } else
+            {
+                logger.LogInformation("@@@is Not valid");
+            }
+
+                return View(board);
+        }
+    }
+}
---
+++ Controllers/Limeboard/MemberController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+
+namespace WebApplication1.Controllers.Limeboard
+{
+    public class MemberController : Controller
+    {
+        private readonly MyAppContext _context;
+
+        public MemberController(MyAppContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var Member = await _context.Members.Include(m => m.Boards)
+                .ToListAsync();
+
+            return View(Member);
+        }
+    }
+}
