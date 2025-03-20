bf2f6fa335eb8a25c8f40653e8cb57affa93a8d5
Juhyorim <joohr1234@gmail.com>
2025-03-21 01:30:56 +0900

    feat: 게시글 자세히 보기 기능 추가

--- Controllers/Limeboard/BoardController.cs
+++ Controllers/Limeboard/BoardController.cs
@@ -3,13 +3,14 @@ using Microsoft.EntityFrameworkCore;
 using WebApplication1.Data;
 using WebApplication1.Models.limeboard;
 using Microsoft.Extensions.Logging;
+using System.Diagnostics;
 
 
 namespace WebApplication1.Controllers.Limeboard
 {
     public class BoardController : Controller
     {
-        private readonly ILoggerFactory factory = LoggerFactory.Create(builder => builder.AddConsole());
+        //private readonly ILoggerFactory factory = LoggerFactory.Create(builder => builder.AddConsole());
 
         private readonly MyAppContext _context;
 
@@ -25,6 +26,12 @@ namespace WebApplication1.Controllers.Limeboard
             return View(Boards);
         }
 
+        public async Task<IActionResult> Info(int id)
+        {
+            var Board = await _context.Boards.Include(b => b.Member).FirstOrDefaultAsync(b => b.Id == id);
+            return View(Board);
+        }
+
         public IActionResult Create()
         {
             return View();
@@ -33,24 +40,24 @@ namespace WebApplication1.Controllers.Limeboard
         [HttpPost]
         public async Task<IActionResult> Create([Bind("Title, Content")] Board board)
         {
-            ILogger logger = factory.CreateLogger("Program");
+            //ILogger logger = factory.CreateLogger("Program");
 
-            board.MemberId = 1;
+            board.MemberId = 1; //no login dummy
 
 
             if (ModelState.IsValid)
             {
-                logger.LogInformation("@@@@@@isValid");
+                Debug.WriteLine("@@@@@@isValid");
 
                 _context.Boards.Add(board);
                 await _context.SaveChangesAsync();
                 return RedirectToAction("Index");
             } else
             {
-                logger.LogInformation("@@@is Not valid");
+                Debug.WriteLine("@@@is Not valid");
             }
 
-                return View(board);
+             return View(board);
         }
     }
 }
