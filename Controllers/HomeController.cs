7cbd2c7dcf0153a77ea5732db675d7bef86d7075
Juhyorim <joohr1234@gmail.com>
2025-03-21 17:14:10 +0900

    feat: 세션사용 디폴트 auth 기능 구현

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,20 +1,26 @@
 using System.Diagnostics;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Areas.Identity.Data;
 using WebApplication1.Models;
 
 namespace WebApplication1.Controllers;
 
+//[Authorize] only authorized user can access
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly UserManager<ApplicationUser> _userManager;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, UserManager<ApplicationUser> userManager)
     {
         _logger = logger;
+        this._userManager = userManager;
     }
 
     public IActionResult Index()
     {
+        ViewData["UserId"] = _userManager.GetUserId(this.User);
         return View();
     }
 
--- Program.cs
+++ Program.cs
@@ -1,12 +1,26 @@
 using Microsoft.EntityFrameworkCore;
 using WebApplication1.Data;
+using Microsoft.AspNetCore.Identity;
+using WebApplication1.Areas.Identity.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//builder.Services.AddDbContext<MyAppContext>(options =>
+//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
+
+builder.Services.AddDbContext<AuthDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
+//문자열을 defaultconnectionString으로 안하면 안뜨네 아
+
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddEntityFrameworkStores<AuthDbContext>();
+
+
 // Add services to the container.
 builder.Services.AddControllersWithViews(); //add service to the DI container
-builder.Services.AddDbContext<MyAppContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
+
+
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
@@ -28,5 +42,7 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
 
 app.Run();
